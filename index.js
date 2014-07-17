083a9688869fe6c6ffc14105548db206372208fe
John Brett <johnbrett7@gmail.com>
2014-07-18 00:40:55 +0100

    Added authentication and better documentation

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
 var Hapi = require('hapi');
-var db = require('level');
 
 var server = Hapi.createServer('localhost', 8080, {
     cors: true
 });
 
-server.pack.register([
-    {
-        plugin: require("hapi-level"),
-        options: {
-            path: './db',
-            config: {
-                valueEncoding: 'json'
+server.pack.register(require('hapi-auth-bearer-token'), function(err){
+
+    server.auth.strategy('default', 'bearer-access-token', 'required', {
+        validateFunc: function(token, callback) {
+            callback(null, token === "abcdef1234", { token: token })
+        }
+    })
+
+    server.pack.register([
+        {
+            plugin: require("hapi-level"),
+            options: {
+                path: './db',
+                config: {
+                    valueEncoding: 'json'
+                }
+            }
+        },
+        {
+            plugin: require('hapi-swagger'),
+            options: {
+                basePath: 'http://localhost:8080',
+                endpoint: '/docs',
+                pathPrefixSize: 1,
+                apiVersion: 1,
+                auth: false
+            }
+        },
+        {
+            plugin: require("./plugins/user")
+        },
+        {
+            plugin: require("./plugins/organisation")
+        },
+        {
+            plugin: require("./plugins/documentation"),
+            options: {
+                basePath: 'http://localhost:8080',
+                endpoint: '/docs',
+                auth: false,
+                alias: '/documentation'
             }
         }
-    },
-    {
-        plugin: require('hapi-swagger'),
-        options: {
-            basePath: 'http://localhost:8080',
-            endpoint: '/docs',
-            pathPrefixSize: 1,
-            apiVersion: 1
-        }
-    },
-    {
-        plugin: require("./plugins/user")
-    },
-    {
-        plugin: require("./plugins/organisation")
-    }
-], function() {
-    server.start(function () {
-        console.log('Server started at: ' + server.info.uri);
+    ], function() {
+        server.start(function () {
+            console.log('Server started at: ' + server.info.uri);
+        })
     })
-})
 
+})
 
 
---
+++ plugins/documentation/index.js
@@ -0,0 +1,47 @@
+exports.register = function(plugin, options, next) {
+
+    plugin.path(__dirname);
+
+    plugin.dependency('hapi-swagger', function(plugin, next){
+
+        plugin.views({
+            engines: { html: require('handlebars') },
+            path: './public'
+        })
+
+        plugin.route([
+            {
+                method: 'GET',
+                path: options.alias || 'documentation',
+                config: {
+                    handler: function(request, reply){
+                        reply.view('index.html', {
+                            title: 'D4H API Documentation',
+                            markdown: '<p>More Info about the documentation will be posted here...</p>',
+                            settings:{
+                                basePath: options.basePath,
+                                endpoint: options.endpoint
+                            }
+                        });
+                    },
+                    auth: options.auth || false
+                }
+            }
+        ])
+
+        next();
+    })
+
+    next();
+}
+
+exports.register.attributes = {
+    pkg: {
+        "name": "documentation",
+        "version": "0.0.1",
+        "description": "This is the documentation provider",
+        "main": "index.js",
+        "author": "John Brett",
+        "license": "MIT"
+    }
+}
