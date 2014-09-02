49959b688976b0354f85749697997a524c8b9fff
John Brett <johnbrett7@gmail.com>
2014-09-02 22:49:35 +0100

    move authentication into a plugin

--- index.js
+++ index.js
@@ -5,64 +5,41 @@ var server = Hapi.createServer('localhost', process.env.PORT || 8080, {
 });
 
 server.pack.register([
-        { plugin: require('hapi-auth-bearer-token') },
-        {
-            plugin: require("hapi-level"),
-            options: {
-                path: './db',
-                config: {
-                    valueEncoding: 'json'
-                }
+    { plugin: require('hapi-auth-bearer-token') },
+    { plugin: require('./plugins/authentication') },
+    {
+        plugin: require("hapi-level"),
+        options: {
+            path: './db',
+            config: {
+                valueEncoding: 'json'
             }
         }
-    ], function(err){
-
-
-    var auth_scheme = process.env.AUTH || 'optional'
-    server.auth.strategy('default', 'bearer-access-token', auth_scheme, {
-        validateFunc: function(token, callback) {
-            var db = server.plugins['hapi-level'].db
-            var users = db.sublevel('users')
-
-            var response = []
-            users.createReadStream()
-                .on('data', function(data) {
-                    if(data.value.access_token === token) {
-                        callback(null, true, { token: token, user: data.value })
-                    }
-                })
-                .on('end', function(data) {
-                    callback(null, false, null)
-                })
+    },
+    {
+        plugin: require('hapi-swagger'),
+        options: {
+            basePath: server.info.uri,
+            endpoint: '/docs',
+            pathPrefixSize: 1,
+            apiVersion: 1,
+            auth: false,
+            payloadType: 'form'
         }
-    })
-
-    server.pack.register([
-        {
-            plugin: require('hapi-swagger'),
-            options: {
-                basePath: server.info.uri,
-                endpoint: '/docs',
-                pathPrefixSize: 1,
-                apiVersion: 1,
-                auth: false,
-                payloadType: 'form'
-            }
-        },
-        {
-            plugin: require("./plugins/documentation"),
-            options: {
-                basePath: server.info.uri,
-                endpoint: '/docs',
-                auth: false,
-                alias: '/documentation'
-            }
-        },
-        { plugin: require("./plugins/user") },
-        { plugin: require("./plugins/organisation") }
-    ], function() {
-        server.start(function () {
-            console.log('Server started at: ' + server.info.uri);
-        })
+    },
+    {
+        plugin: require("./plugins/documentation"),
+        options: {
+            basePath: server.info.uri,
+            endpoint: '/docs',
+            auth: false,
+            alias: '/documentation'
+        }
+    },
+    { plugin: require("./plugins/user") },
+    { plugin: require("./plugins/organisation") }
+], function() {
+    server.start(function () {
+        console.log('Server started at: ' + server.info.uri);
     })
 })
---
+++ plugins/authentication/index.js
@@ -0,0 +1,34 @@
+exports.register = function(plugin, options, next) {
+
+    var auth_scheme = process.env.AUTH || 'optional'
+    
+    plugin.auth.strategy('default', 'bearer-access-token', auth_scheme, {
+        validateFunc: function(token, callback) {
+            var db = server.plugins['hapi-level'].db
+            var users = db.sublevel('users')
+
+            var response = []
+            users.createReadStream()
+                .on('data', function(data) {
+                    if(data.value.access_token === token) {
+                        callback(null, true, { token: token, user: data.value })
+                    }
+                })
+                .on('end', function(data) {
+                    callback(null, false, null)
+                })
+        }
+    })
+
+    next();
+}
+
+exports.register.attributes = {
+    pkg: {
+        "name": "authentication",
+        "description": "This is the authentication provider",
+        "main": "index.js",
+        "author": "John Brett",
+        "license": "MIT"
+    }
+}
